586b8894f1df81eb69ed2aba46f8eee7abb4a3df
onion <kosukekito38@gmail.com>
2020-10-24 11:33:55 +0900

    initial commit

--- mount_folder/HOPwR/proj-1/roll.R
+++
@@ -1,9 +0,0 @@
-roll <- function(prob) {
-  die = 1:6
-  dice = sample(die, size = 2, replace = T, prob = prob)
-  sum(dice)
-}
-roll2 <- function(die) {
-  dice = sample(die, size = 2, replace = T)
-  sum(dice)
-}
--- mount_folder/HOPwR/proj-2/show_env.R
+++
@@ -1,7 +0,0 @@
-show_env <- function() {
-  list(
-    ran.in = environment(),
-    parent = parent.env(environment()),
-    objects = ls.str(environment())
-  )
-}
